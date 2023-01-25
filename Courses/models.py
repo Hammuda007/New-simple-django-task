8f55be84f9f65ef02b7a001f4709c613d43b70a7
Hammuda007 <sheakho19@gmail.com>
2023-01-25 01:00:38 +0100

    update code

--- Courses/models.py
+++ Courses/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
 class Cours(models.Model):
     title=models.CharField(max_length=100)
     content=models.TextField(max_length=100)
-    suptitle=models.TextField(max_length=100)
+    suptitle=models.CharField(max_length=100)
     Description=models.TextField(max_length=10000)
     Requirment=models.TextField(max_length=1000)
     image=models.ImageField(upload_to='Courses/', default='default.png')
+    tags = TaggableManager()
    
 
     def __str__(self):
